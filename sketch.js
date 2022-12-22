48729c1b76b01cf3e43a2a23fdc04f624b344597
William Zhou <williamz9468@gmail.com>
2022-12-21 17:39:04 -0800

    init

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var size = 30
+
+
+function setup() {
+  var canvas=createCanvas(windowWidth, windowHeight);
+  canvas.parent(sketch)
+}
+
+
+function draw() {
+  stroke(200)
+  background('white');
+  
+  for(var i=-1; i<20;i++){
+    
+    for (var x = 0; x<width;x+=size*2.5){
+      fill('white')
+      
+      rect(x,i*size*2.5,size,size)
+
+    }
+    for (var x = 0; x<width;x+=size*2.5){
+      fill('white')
+      rect(x+size,(0.5+(i*2.5))*size,size,size)
+
+    }
+    for (var x = 0; x<width;x+=size*2.5){
+      fill('white')
+      rect(x-size*0.5,(1+(i*2.5))*size,size,size)
+
+    }
+    for (var x = 0; x<width;x+=size*2.5){
+      fill('white')
+      rect(x+size*0.5,(1.5+(i*2.5))*size,size,size)
+
+    }
+    for (var x = 0; x<width;x+=size*2.5){
+      fill('white')
+      rect(x-size,(2+(i*2.5))*size,size,size)
+
+    }
+    
+    
+    
+  }
+  
+}
+v
