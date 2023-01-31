7561eea30e02175e3f46509ab26ff5ead8d3ebfa
William Zhou <williamz9468@gmail.com>
2023-01-30 22:26:15 -0800

    add: preview

--- sketch.js
+++ sketch.js
@@ -1,50 +1,92 @@
-var size = 30
+var points = []
+var multi = 0.005
+
+
+var r1
+var r2
+var g1
+var g2
+var b1
+var b2
 
 
 function setup() {
   var canvas=createCanvas(windowWidth, windowHeight);
   canvas.parent(sketch)
+  background(30)
+  angleMode(DEGREES)
+  noiseDetail(1)
+  
+  
+  var density = 30
+  var space = width/density
+  
+  for(var x = 0; x<width;x+=space){
+    for (var y = 0; y<height;y+=space){
+      var p = createVector(x + random(-10,10),y + random(-10,10))
+      points.push(p)
+    }
+  }
+  
+  shuffle(points,true)
+  
+r1 = random(255)
+r2 = random(255)
+g1 = random(255)
+g2 = random(255)
+b1 = random(255)
+b2 = random(255)
+  
+multi = random(0.002,0.01)
 }
 
 
 function draw() {
-  stroke(200)
-  background('white');
+  noStroke()
   
-  for(var i=-1; i<20;i++){
+  if (frameCount *2 <= points.length){
+    var max = 400
+  }
+  else{
+    var max = points.length
+  }
+  
+  
+  for(var i = 0; i<max; i++){
     
-    for (var x = 0; x<width;x+=size*2.5){
-      fill('white')
+    var r = map(points[i].x,0,width,r1,r2)
+    var g = map(points[i].y,0,height,g1,g2)
+    var b = map(points[i].x,0,width,b1,b2)
+    var alpha = map(dist(width/2, height / 2, points[i].x, points[i].y),0,350, 400, 0)
+    
+    fill(r,g,b, alpha)
+    var angle = map(noise(points[i].x * multi ,points[i].y * multi),0,1,0,720)
+    
+    points[i].add(createVector(cos(angle),sin(angle)))
+    
+    
+    if (dist(width/2,height/2,points[i].x,points[i].y)  <  350  ) {
       
-      rect(x,i*size*2.5,size,size)
-
+      
+    
+     Math.random()  >0.5? rect(points[i].x,points[i].y,random(5),random(5)):ellipse(points[i].x,points[i].y,1)
     }
-    for (var x = 0; x<width;x+=size*2.5){
-      fill('white')
-      rect(x+size,(0.5+(i*2.5))*size,size,size)
-
-    }
-    for (var x = 0; x<width;x+=size*2.5){
-      fill('white')
-      rect(x-size*0.5,(1+(i*2.5))*size,size,size)
-
-    }
-    for (var x = 0; x<width;x+=size*2.5){
-      fill('white')
-      rect(x+size*0.5,(1.5+(i*2.5))*size,size,size)
-
-    }
-    for (var x = 0; x<width;x+=size*2.5){
-      fill('white')
-      rect(x-size,(2+(i*2.5))*size,size,size)
-
-    }
+    
+    
+    
+    
+    
+    
+    
     
     
     
   }
+    
+  
   
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+
 }
